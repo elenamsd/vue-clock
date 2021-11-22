4b1a4c5a16549581e3d2cd8364a7b16215cc28fd
Juan Fran Martin <juanframaro@gmail.com>
2021-11-22 20:05:39 +0000

    implemented time function

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,20 +1,29 @@
 <template>
-  <h1>Soy un reloj, mi hora es: {{ date }}</h1>
+  <h1>Soy un reloj, mi hora es: {{ time }}</h1>
 </template>
 
 
 <script>
+import dayjs from "dayjs";
 export default {
   name: "Clock",
   data() {
     return {
-      date: this.getDate()
+      date: this.getDate(),
+      time: this.getTime()
     }
 
   },
   methods: {
     getDate() {
-      return new Date();
+      return new Date()
+    },
+    getTime(){
+      const date = dayjs();
+      const hour= date.hour().toString().padStart(2,'0');
+      const minute= date.minute().toString().padStart(2,'0');
+      const second = date.second().toString().padStart(2,'0');
+      return `${hour}:${minute}:${second}`;
     }
   }
 }
