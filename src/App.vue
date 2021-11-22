c2ed60f538fc380b1632027eca424e38afa8c2e7
Elena Sacramento <elenasacramento80@gmail.com>
2021-11-22 19:42:08 +0000

    Creating clock component 🕔

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Hola</h1>
+  <Clock></Clock>
+</template>
+
+
+<script>
+import Clock from "./components/Clock.vue"
+export default {
+  name: "App",
+  components: {
+    Clock
+  },
+  data() {
+
+  }
+}
+</script>
---
+++ src/components/Clock.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Soy un reloj, mi hora es: {{ date }}</h1>
+</template>
+
+
+<script>
+export default {
+  name: "Clock",
+  data() {
+    return {
+      date: this.getDate()
+    }
+
+  },
+  methods: {
+    getDate() {
+      return new Date();
+    }
+  }
+}
+</script>
